52f0267f9f3447051f6379ddd4d8bde9008e99b3
whalebone7 <125891350+whalebone7@users.noreply.github.com>
2023-10-30 18:56:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"math/rand"
 	"time"
 	"net/url"
+	"crypto/tls"
 )
 
 func main() {
@@ -36,42 +37,51 @@ func main() {
 }
 
 func processURL(userAgent string, parsedURL *url.URL) {
-    client := &http.Client{}
-    req, err := http.NewRequest("GET", parsedURL.String(), nil)
-    if err != nil {
-        fmt.Printf("Error creating the request: %v\n", err)
-        return
-    }
-    req.Header.Set("User-Agent", userAgent)
+	
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
 
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Printf("Error sending the request: %v\n", err)
-        return
-    }
-    defer resp.Body.Close()
+	req, err := http.NewRequest("GET", parsedURL.String(), nil)
+	if err != nil {
+		fmt.Printf("Error creating the request: %v\n", err)
+		return
+	}
+	req.Header.Set("User-Agent", userAgent)
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Printf("Error reading the response: %v\n", err)
-        return
-    }
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending the request: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
 
-    injectedValues := []string{"{{7878*582}}", "<%= 7878*582 %>", "${{7878*582}}", "#{7878*582}", "*{7878*582}", "${7878*582}."}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading the response: %v\n", err)
+		return
+	}
 
-    if strings.Contains(string(body), "4584996") {
-        fmt.Printf("URL: %s\n", parsedURL)
-        fmt.Println("4584996 found in the response!\n")
-    }
+	injectedValues := []string{"{{7878*582}}", "<%= 7878*582 %>", "${{7878*582}}", "#{7878*582}", "*{7878*582}", "${7878*582}."}
+
+	if strings.Contains(string(body), "4584996") {
+		fmt.Printf("URL: %s\n", parsedURL)
+		fmt.Printf("Parameter: %s\n", parsedURL.RawQuery)
+		fmt.Printf("Injected Values:\n")
+		for _, value := range injectedValues {
+			fmt.Printf("%s: %s\n", value, value)
+		}
+		fmt.Println("4584996 found in the response!\n")
+	}
 }
-
 
 func generateRandomUserAgent() string {
 	userAgents := []string{
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/52.0",
 		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Safari/602.4.8",
-		
 	}
 
 	rand.Seed(time.Now().UnixNano())
